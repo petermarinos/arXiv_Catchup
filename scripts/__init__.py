f744b21a8cb2f937afc1495b58fd231d4cfb9a71
petermarinos <peter_marinos@hotmail.com.au>
2026-02-22 00:47:06 -0800

    Added docstring

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,1 +1,12 @@
+"""arXiv_Catchup
+
+Included modules:
+ArxivClient : Connects to arXiv servers
+CLI         : All CLI responsibilities
+Config      : Configuration for the pipeline
+Corpus      : Contains all papers
+Paper       : Stores all information for a paper
+UI          : All UI responsibilities
+"""
+
 __version__ = "1.7.0"
