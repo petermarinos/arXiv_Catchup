1d97ba234152fcf5ca0810f221e074a1a04b859f
petermarinos <peter_marinos@hotmail.com.au>
2026-03-03 15:31:04 -0600

    Added tests
    Now test all possibilities:
    1. Non-retryable HTTP error
    2. Retryable HTTP error
    3. Retryable HTTP error with missing retry-after header
    4.

--- tests/test_http.py
+++ tests/test_http.py
@@ -2,62 +2,200 @@
 
 # Import libraries
 from email.message import Message
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 import unittest
+import ssl
+import urllib.error
+import xml.etree.ElementTree as ET
 
 # Import functions to test
-# from scripts.string_handling import authors_match, words_match_pattern
+from scripts.http_client import HttpClient, TooManyAttempts
 
 
 class TestHttpRequests(unittest.TestCase):
-    """Test the http requests and error handling."""
+    """Test the http requests and error handling.
+    NOTE: Patch the prett_sleep function for all tests to prevent unnecessary sleeps.
+    """
 
+    def setUp(self) -> None:
+        self.url_template = "http://example.test/?start={start_num}&max={blocksize}"
 
-# Test error handling
-# Add an indent to the "attempt to connect to arXiv"--"return parsed_xml_date" lines
-# -- add proper testing in the future
+    @patch("scripts.http_client.pretty_sleep")
+    def test_arxiv_query_success(self, _sleep_mock: float):
+        """Test that the arxiv_query returns a result if there are no errors."""
 
-# # HTTP ERRORS
+        # Prepare a fake response context manager
+        response = MagicMock()
+        response.read.return_value = b"<feed></feed>"
+        cm = MagicMock()
+        cm.__enter__.return_value = response
 
-# # http error with a random code and no header
-# err = urllib.error.HTTPError(
-#     url=None, code=47, msg="fake error that should exit", hdrs=None, fp=None
-# )
-# with patch("urllib.request.urlopen", side_effect=err):
+        with patch(
+            "scripts.http_client.urllib.request.urlopen", return_value=cm
+        ) as mock_urlopen, patch(
+            "scripts.http_client.convert_request_to_xml_root",
+            return_value=ET.Element("feed"),
+        ) as mock_convert:
 
-# # http repeating error code with no header
-# err = urllib.error.HTTPError(
-#     url=None, code=408, msg="non-repeating code", hdrs=None, fp=None
-# )
-# with patch("urllib.request.urlopen", side_effect=err):
+            client = HttpClient()
+            result = client.arxiv_query(self.url_template, 0, 1)
 
-# # http non-repeating error code with a useless header
-# headers = Message()
-# headers["blank"] = "nothing"
-# err = urllib.error.HTTPError(
-#     url=None, code=408, msg="non-repeating code", hdrs=headers, fp=None
-# )
-# with patch("urllib.request.urlopen", side_effect=err):
+            # validate return and that urlopen was called
+            self.assertIsInstance(result, ET.Element)
+            mock_urlopen.assert_called()
+            mock_convert.assert_called()
 
-# # http error with a retry-after header
-# headers = Message()
-# headers["Retry-After"] = 40
-# err = urllib.error.HTTPError(
-#     url=None, code=429, msg="repeating code", hdrs=headers, fp=None
-# )
-# with patch("urllib.request.urlopen", side_effect=err):
+    @patch("scripts.http_client.pretty_sleep")
+    def test_http_error_non_retryable(self, _sleep_mock: float):
+        """Test that a non-retryable HTTPError should raise RuntimeError."""
 
-# # URL ERRORS
+        # Define some empty headers
+        headers = Message()
+        err = urllib.error.HTTPError("None_URL", 400, "bad", headers, None)
 
-# # non-Verification errors
-# with patch(
-#     "urllib.request.urlopen", side_effect=urllib.error.URLError("DNS fail")
-# ):
+        with patch("scripts.http_client.urllib.request.urlopen", side_effect=err):
+            client = HttpClient()
 
-# # Verification errors
-# err = urllib.error.URLError(
-#     ssl.SSLCertVerificationError(
-#         "certificate verify failed: unable to get local issuer certificate"
-#     )
-# )
-# with patch("urllib.request.urlopen", side_effect=err):
+            # Should hit the RuntimeError for non-retryable errors
+            with self.assertRaises(RuntimeError):
+                client.arxiv_query(self.url_template, 0, 1)
+
+    @patch("scripts.http_client.pretty_sleep")
+    def test_http_error_retryable(self, _sleep_mock: float):
+        """Test that a retryable HTTPError will re-attempt the connection until the maximum attempt
+        limit is reached.
+        """
+
+        # Define some empty headers
+        headers = Message()
+        err = urllib.error.HTTPError("None_URL", 408, "request timeout", headers, None)
+
+        with patch("scripts.http_client.urllib.request.urlopen", side_effect=err):
+            client = HttpClient()
+
+            # The same error is injected for all attempts.
+            # Should reach the custom TooManyAttempts raise
+            with self.assertRaises(TooManyAttempts):
+                client.arxiv_query(self.url_template, 0, 1)
+
+    @patch("scripts.http_client.pretty_sleep")
+    def test_http_error_429_without_retry(self, _sleep_mock: float):
+        """Test that a 429 HTTPError without a Retry-After command will increase the wait time to 90
+        seconds.
+        """
+
+        # Require that a header is returned by the error
+        headers = Message()
+        headers["mock entry"] = "nothing of note"
+        err = urllib.error.HTTPError("None_URL", 429, "too many", headers, None)
+
+        # successful response context
+        response = MagicMock()
+        response.read.return_value = b"<feed></feed>"
+        cm = MagicMock()
+        cm.__enter__.return_value = response
+
+        side_effects: list[urllib.error.HTTPError | MagicMock] = [err, cm]
+
+        with patch(
+            "scripts.http_client.urllib.request.urlopen", side_effect=side_effects
+        ) as _mock_urlopen, patch(
+            "scripts.http_client.convert_request_to_xml_root",
+            return_value=ET.Element("feed"),
+        ):
+
+            client = HttpClient()
+            result = client.arxiv_query(self.url_template, 0, 1)
+
+            self.assertIsInstance(result, ET.Element)
+            self.assertEqual(client.retry_after, 90.0)
+
+    @patch("scripts.http_client.pretty_sleep")
+    def test_http_error_429_with_retry(self, _sleep_mock: float):
+        """Test that the if server replies with a retryable HTTPError that includes a Retry-After
+        command, that the client will wait for the requested amount of time.
+        """
+
+        test_retry_value = "5"
+
+        headers = Message()
+        headers["Retry-After"] = "5"
+        err = urllib.error.HTTPError("None_URL", 429, "too many", headers, None)
+
+        # successful response context
+        response = MagicMock()
+        response.read.return_value = b"<feed></feed>"
+        cm = MagicMock()
+        cm.__enter__.return_value = response
+
+        side_effects: list[urllib.error.HTTPError | MagicMock] = [err, cm]
+
+        with patch(
+            "scripts.http_client.urllib.request.urlopen", side_effect=side_effects
+        ) as _mock_urlopen, patch(
+            "scripts.http_client.convert_request_to_xml_root",
+            return_value=ET.Element("feed"),
+        ):
+
+            client = HttpClient()
+            result = client.arxiv_query(self.url_template, 0, 1)
+
+            self.assertIsInstance(result, ET.Element)
+            self.assertEqual(client.retry_after, float(test_retry_value))
+
+    @patch("scripts.http_client.pretty_sleep")
+    def test_cert_url_error(self, _sleep_mock: float):
+        """Test that a certificate verification URLError updates the cert_error_bool flag (and
+        therefore the ssl certificate).
+        """
+
+        ssl_err = ssl.SSLCertVerificationError("certificate verify failed")
+        err = urllib.error.URLError(ssl_err)
+
+        # successful response context after first failure
+        response = MagicMock()
+        response.read.return_value = b"<feed></feed>"
+        cm = MagicMock()
+        cm.__enter__.return_value = response
+
+        side_effects: list[urllib.error.HTTPError | MagicMock] = [err, cm]
+
+        with patch(
+            "scripts.http_client.urllib.request.urlopen", side_effect=side_effects
+        ) as _mock_urlopen, patch(
+            "scripts.http_client.convert_request_to_xml_root",
+            return_value=ET.Element("feed"),
+        ):
+
+            client = HttpClient()
+            result = client.arxiv_query(self.url_template, 0, 1)
+
+            self.assertIsInstance(result, ET.Element)
+            self.assertTrue(client.cert_error_bool)
+
+    @patch("scripts.http_client.pretty_sleep")
+    def test_timeout_error(self, _sleep_mock: float):
+        """Test that a timeout error correctly updates the retry timer to 60 seconds."""
+
+        err = TimeoutError
+
+        # successful response context
+        response = MagicMock()
+        response.read.return_value = b"<feed></feed>"
+        cm = MagicMock()
+        cm.__enter__.return_value = response
+
+        side_effects: list[urllib.error.HTTPError | MagicMock] = [err, cm]
+
+        with patch(
+            "scripts.http_client.urllib.request.urlopen", side_effect=side_effects
+        ) as _mock_urlopen, patch(
+            "scripts.http_client.convert_request_to_xml_root",
+            return_value=ET.Element("feed"),
+        ):
+
+            client = HttpClient()
+            result = client.arxiv_query(self.url_template, 0, 1)
+
+            self.assertIsInstance(result, ET.Element)
+            self.assertEqual(client.retry_after, 60.0)
