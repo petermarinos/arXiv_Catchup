07246df5d362c1f2f2106ea595b2420d1b1b1e9c
petermarinos <peter_marinos@hotmail.com.au>
2026-03-13 14:17:02 -0700

    Added dataclass to hold runner objects

--- src/arxiv_catchup/gui/controller.py
+++ src/arxiv_catchup/gui/controller.py
@@ -29,6 +29,41 @@ from arxiv_catchup.dates import parse_date, split_date
 from arxiv_catchup.utils import logger_setup
 
 
+class ApiNotInitialised(Exception):
+    """Exception raised if the API has not been initialised yet.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+@dataclass
+class Runner:
+    """Dataclass to hold the objects used to run the logic."""
+
+    cli: CLI
+    storage: Storage
+    search_params: Config
+    corpus: Corpus
+
+    http_client: HttpClient
+    api: ArxivClient | None = None
+
+    def get_api(self) -> ArxivClient:
+        """Basic function to return the API. Ensures the type checker knows what is happening, and
+        prevents potential issues if the user is somehow able to call the API before it is
+        initalised.
+        """
+
+        if self.api is None:
+            raise ApiNotInitialised("API has not been initialised.")
+        return self.api
+
+
 @dataclass
 class GuiState:
     """Small dataclass to hold the states of the GUI.
@@ -68,26 +103,25 @@ class Controller:
 
         self.state = GuiState()
 
-        self.cli = CLI()
-        self.storage = Storage()
+        self.runner = Runner(
+            cli=CLI(),
+            storage=Storage(),
+            search_params=Config(),
+            corpus=Corpus(),
+            http_client=HttpClient(),
+        )
 
-        logger_setup(self.cli.args, self.storage.paths.log)
+        logger_setup(self.runner.cli.args, self.runner.storage.paths.log)
         self.logger = logging.getLogger(__name__)
-        self.cli.add_logger()
-        self.storage.add_logger()
+        self.runner.cli.add_logger()
+        self.runner.storage.add_logger()
 
-        self.search_params = Config()
-        self.search_params.get_searchterms(self.storage)
-        self.search_params.get_dates(self.storage, self.cli.args)
-
-        self.http_client = HttpClient()
-        self.api: ArxivClient
-
-        self.corpus = Corpus()
+        self.runner.search_params.get_searchterms(self.runner.storage)
+        self.runner.search_params.get_dates(self.runner.storage, self.runner.cli.args)
 
         # Create GUI and wire callbacks
         self.gui = GUI(
-            self.search_params,
+            self.runner.search_params,
             on_keep_temp=self.on_keep_temp,
             on_check_dates=self.on_check_dates,
             on_search_info=self.on_search_info,
@@ -107,32 +141,34 @@ class Controller:
 
         # Once the GUI has been closed:
         try:
-            self.storage.delete_temp_files(self.cli.args.keep_temp)
+            self.runner.storage.delete_temp_files(self.runner.cli.args.keep_temp)
         except FileNotFoundError:
             self.logger.debug("Attempted to delete temporary files that did not exist.")
 
-        self.storage.write_aux_files(self.search_params.end_date, self.corpus.length)
+        self.runner.storage.write_aux_files(
+            self.runner.search_params.end_date, self.runner.corpus.length
+        )
 
         # # Print a summary
-        self.corpus.summary()
+        self.runner.corpus.summary()
 
     def on_keep_temp(self) -> None:
         """WIP"""
 
         self.logger.debug("Toggled keep_temp checkbox.")
 
-        self.cli.args.keep_temp = self.gui.get_bool_from_checkbox("keep_temp")
+        self.runner.cli.args.keep_temp = self.gui.get_bool_from_checkbox("keep_temp")
 
-        self.logger.debug("keep_temp set to %s", self.cli.args.keep_temp)
+        self.logger.debug("keep_temp set to %s", self.runner.cli.args.keep_temp)
 
     def on_new_window(self) -> None:
         """WIP"""
 
         self.logger.debug("Toggled new_window checkbox.")
 
-        self.cli.args.new_window = self.gui.get_bool_from_checkbox("new_window")
+        self.runner.cli.args.new_window = self.gui.get_bool_from_checkbox("new_window")
 
-        self.logger.debug("new_window set to %s", self.cli.args.new_window)
+        self.logger.debug("new_window set to %s", self.runner.cli.args.new_window)
 
     def on_check_dates(self) -> None:
         """Callback for the 'Check Search Dates' button."""
@@ -141,32 +177,37 @@ class Controller:
 
         try:
 
-            temp_start_date = self.search_params.start_date
-            temp_end_date = self.search_params.end_date
+            temp_start_date = self.runner.search_params.start_date
+            temp_end_date = self.runner.search_params.end_date
 
-            self.search_params.start_time, self.search_params.start_date = parse_date(
+            (
+                self.runner.search_params.start_time,
+                self.runner.search_params.start_date,
+            ) = parse_date(
                 self.logger,
                 self.gui.get_date_from_entry("start_date"),
                 "start-date",
-                self.search_params.SEARCH_TIME,
-                self.search_params.POST_TIME,
+                self.runner.search_params.SEARCH_TIME,
+                self.runner.search_params.POST_TIME,
             )
 
-            self.search_params.end_time, self.search_params.end_date = parse_date(
-                self.logger,
-                self.gui.get_date_from_entry("end_date"),
-                "end-date",
-                self.search_params.SEARCH_TIME,
-                self.search_params.POST_TIME,
+            self.runner.search_params.end_time, self.runner.search_params.end_date = (
+                parse_date(
+                    self.logger,
+                    self.gui.get_date_from_entry("end_date"),
+                    "end-date",
+                    self.runner.search_params.SEARCH_TIME,
+                    self.runner.search_params.POST_TIME,
+                )
             )
 
-            self.search_params.date_error_check()
+            self.runner.search_params.date_error_check()
 
             # If the dates changed since the previous check, disable all GUI buttons other than
             #     'search_info' and continue to after the except
             if (
-                temp_start_date != self.search_params.start_date
-                or temp_end_date != self.search_params.end_date
+                temp_start_date != self.runner.search_params.start_date
+                or temp_end_date != self.runner.search_params.end_date
             ):
                 self.gui.button_set_state("download_papers", False)
                 self.gui.button_set_state("score_papers", False)
@@ -190,8 +231,8 @@ class Controller:
 
         # self.state.dates_checked = True
         self.logger.info("Dates checked and are valid.")
-        s_yyyy, s_mm, s_dd = split_date(self.search_params.start_date)
-        e_yyyy, e_mm, e_dd = split_date(self.search_params.end_date)
+        s_yyyy, s_mm, s_dd = split_date(self.runner.search_params.start_date)
+        e_yyyy, e_mm, e_dd = split_date(self.runner.search_params.end_date)
         date_string = (
             f"Searching from {s_yyyy}-{s_mm}-{s_dd} 19:00 UTC "
             f"to {e_yyyy}-{e_mm}-{e_dd} 19:00 UTC"
@@ -201,16 +242,18 @@ class Controller:
         self.gui.button_set_state("search_info", True)
 
         # If the search information is good, then the ArXiv client can be created
-        self.api = ArxivClient(self.search_params, self.http_client)
+        self.runner.api = ArxivClient(
+            self.runner.search_params, self.runner.http_client
+        )
 
         # Ensure the corpus is empty in case the search dates were changed after downloading
-        self.corpus.clear_corpus()
+        self.runner.corpus.clear_corpus()
 
         self.state.dates_checked = True
 
         # If papers were downloaded previously, then the dates were changed, delete the temp file
         if self.state.papers_downloaded:
-            self.storage.delete_file(self.storage.paths.papers_xml)
+            self.runner.storage.delete_file(self.runner.storage.paths.papers_xml)
 
         self.logger.info("Ready to connect to the arXiv servers.")
 
@@ -221,9 +264,11 @@ class Controller:
 
         try:
 
-            self.api.get_search_info(self.storage)
+            api = self.runner.get_api()
 
-            self.api.arxiv_search_error_check()
+            api.get_search_info(self.runner.storage)
+
+            api.arxiv_search_error_check()
 
         except ArxivError as exc:
 
@@ -261,14 +306,18 @@ class Controller:
 
         # *always ensure the corpus has been cleared first*
         self.logger.debug("Clearing the corpus.")
-        self.corpus.clear_corpus()
+        self.runner.corpus.clear_corpus()
 
         download_cb = self.make_progress_cb("download")
+
+        api = self.runner.get_api()
 
         def worker() -> None:
             """inner function that will download the papers"""
 
-            self.api.get_papers(self.corpus, self.storage, progress_cb=download_cb)
+            api.get_papers(
+                self.runner.corpus, self.runner.storage, progress_cb=download_cb
+            )
 
             # Once get_papers finishes on the thread, then run the last few on-finish functions
             self.gui.after(0, self._on_download_finished)
@@ -289,18 +338,18 @@ class Controller:
 
         self.logger.debug("Selected score_papers button.")
 
-        self.corpus.find_matches(self.search_params.search_terms)
+        self.runner.corpus.find_matches(self.runner.search_params.search_terms)
 
         # Obtain state of the option menu
         score_algo = self.gui.get_state_from_optionmenu("score")
         if score_algo == "Score via Matches":
-            self.cli.args.score_on_matches = True
+            self.runner.cli.args.score_on_matches = True
         elif score_algo == "Score via ML":
-            self.cli.args.score_on_matches = False
+            self.runner.cli.args.score_on_matches = False
         self.logger.debug("Using the %s scoring algorithm.", score_algo)
 
         try:
-            self.cli.score_papers(self.corpus)
+            self.runner.cli.score_papers(self.runner.corpus)
         except NotImplementedError:
             self.logger.error("ML algorithm not yet implemented")
             return
@@ -318,11 +367,11 @@ class Controller:
         # Obtain state of the option menu
         filter_algo = self.gui.get_state_from_optionmenu("filter")
         if filter_algo == "Filter via Score":
-            self.cli.args.filter_on_matches = False
+            self.runner.cli.args.filter_on_matches = False
         elif filter_algo == "Filter via Matches":
-            self.cli.args.filter_on_matches = True
+            self.runner.cli.args.filter_on_matches = True
 
-        self.cli.filter_papers(self.corpus)
+        self.runner.cli.filter_papers(self.runner.corpus)
 
         self.state.papers_filtered = True
 
@@ -335,18 +384,20 @@ class Controller:
 
         self.logger.debug("Selected open_in_browser button.")
 
-        self.cli.open_in_browser = True
-        self.cli.write_to_file = False
+        self.runner.cli.open_in_browser = True
+        self.runner.cli.write_to_file = False
 
         results_cb = self.make_progress_cb("results")
+
+        api = self.runner.get_api()
 
         def worker() -> None:
             """inner function that will open the papers"""
 
-            self.cli.display(
-                self.storage,
-                self.corpus.papers_of_note,
-                self.api.SLEEP_OPENING,
+            self.runner.cli.display(
+                self.runner.storage,
+                self.runner.corpus.papers_of_note,
+                api.SLEEP_OPENING,
                 progress_cb=results_cb,
             )
 
@@ -359,20 +410,22 @@ class Controller:
 
         self.logger.debug("Selected write_to_file button.")
 
-        self.cli.open_in_browser = False
-        self.cli.write_to_file = True
+        self.runner.cli.open_in_browser = False
+        self.runner.cli.write_to_file = True
 
         # Obtain state of the option menu
         write_style = self.gui.get_state_from_optionmenu("write")
         if write_style == "Links":
-            self.cli.args.only_ids = False
+            self.runner.cli.args.only_ids = False
         elif write_style == "ID Numbers":
-            self.cli.args.only_ids = True
+            self.runner.cli.args.only_ids = True
 
-        self.cli.display(
-            self.storage,
-            self.corpus.papers_of_note,
-            self.api.SLEEP_OPENING,
+        api = self.runner.get_api()
+
+        self.runner.cli.display(
+            self.runner.storage,
+            self.runner.corpus.papers_of_note,
+            api.SLEEP_OPENING,
         )
 
     def make_progress_cb(self, key: str) -> Callable[[int, int], None]:
