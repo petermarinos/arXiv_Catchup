319d82eb2691ffb766451ffce7406d6e6ce43cf2
petermarinos <peter_marinos@hotmail.com.au>
2026-03-13 17:05:14 -0700

    Moved helper functions

--- src/arxiv_catchup/gui/gui.py
+++ src/arxiv_catchup/gui/gui.py
@@ -15,6 +15,15 @@ import customtkinter  # pyright: ignore[reportMissingTypeStubs]
 from arxiv_catchup.gui.actions import GuiActions
 from arxiv_catchup.config import Config
 
+# Import project functions
+from arxiv_catchup.gui.gui_elements import (
+    create_button,
+    create_checkbox,
+    create_entry,
+    create_optionmenu,
+    create_progressbar,
+)
+
 
 # Define some small classes. Bounds the expected values and prevents errors within strings.
 class ButtonKind(Enum):
@@ -66,111 +75,6 @@ class ProgressbarKind(Enum):
 
     DOWNLOAD = "download"
     RESULTS = "results"
-
-
-# Helper functions
-def create_button(
-    frame: customtkinter.CTkFrame,
-    width: int,
-    text: str,
-    row_index: int,
-    col_index: int,
-    command: Callable[[], None],
-    disabled: bool = True,
-    col_span: bool = False,
-) -> customtkinter.CTkButton:
-    """WIP"""
-
-    # There are 8 arguments
-    # Want this helper function to have the freedom to control these aspects of the button, and to
-    #     set them to reasonable default values if the attribute is not passed.
-    # Disable pylint for >5 arguments and >5 positional arguments
-    # pylint: disable=R0913
-    # pylint: disable=R0917
-
-    button = customtkinter.CTkButton(
-        master=frame,
-        width=width,
-        text=text,
-        command=command,
-    )
-
-    if col_span:
-        button.grid(row=row_index, columnspan=3, pady=10, padx=10)
-    else:
-        button.grid(row=row_index, column=col_index, pady=10, padx=10)
-
-    if disabled:
-        button.configure(state="disabled")
-
-    return button
-
-
-def create_checkbox(
-    frame: customtkinter.CTkFrame,
-    text: str,
-    row_index: int,
-    col_index: int,
-    command: Callable[[], None],
-) -> customtkinter.CTkCheckBox:
-    """WIP"""
-
-    checkbox = customtkinter.CTkCheckBox(master=frame, text=text, command=command)
-    checkbox.grid(row=row_index, column=col_index, pady=10, padx=10)
-
-    return checkbox
-
-
-def create_entry(
-    frame: customtkinter.CTkFrame,
-    variable_text: str,
-    placeholder_text: str,
-    row_index: int,
-    col_index: int,
-) -> customtkinter.CTkEntry:
-    """WIP"""
-
-    entry = customtkinter.CTkEntry(
-        master=frame,
-        width=220,
-        justify=customtkinter.LEFT,
-        textvariable=customtkinter.StringVar(value=variable_text),
-        placeholder_text=placeholder_text,
-    )
-    entry.grid(row=row_index, column=col_index, pady=10, padx=10)
-
-    return entry
-
-
-def create_progressbar(
-    frame: customtkinter.CTkFrame, row_index: int
-) -> customtkinter.CTkProgressBar:
-    """WIP"""
-
-    progressbar = customtkinter.CTkProgressBar(master=frame)
-    progressbar.grid(row=row_index, columnspan=3, pady=10, padx=10)
-    progressbar.set(0)
-
-    return progressbar
-
-
-def create_optionmenu(
-    frame: customtkinter.CTkFrame,
-    width: int,
-    values: list[str],
-    row_index: int,
-    col_index: int,
-) -> customtkinter.CTkOptionMenu:
-    """WIP"""
-
-    optionmenu = customtkinter.CTkOptionMenu(
-        frame,
-        width=width,
-        values=values,
-    )
-    optionmenu.grid(row=row_index, column=col_index, pady=10, padx=10)
-
-    return optionmenu
 
 
 class GUI:
---
+++ src/arxiv_catchup/gui/gui_elements.py
@@ -0,0 +1,115 @@
+"""Functions that create GUI elements"""
+
+# customtkinter does not have typing fully implemented.
+# Disable all unknown member types for this script
+# pyright: reportUnknownMemberType = false
+
+# Import standard libraries
+from collections.abc import Callable
+
+# Import non-standard libraries
+import customtkinter  # pyright: ignore[reportMissingTypeStubs]
+
+
+def create_button(
+    frame: customtkinter.CTkFrame,
+    width: int,
+    text: str,
+    row_index: int,
+    col_index: int,
+    command: Callable[[], None],
+    disabled: bool = True,
+    col_span: bool = False,
+) -> customtkinter.CTkButton:
+    """WIP"""
+
+    # There are 8 arguments
+    # Want this helper function to have the freedom to control these aspects of the button, and to
+    #     set them to reasonable default values if the attribute is not passed.
+    # Disable pylint for >5 arguments and >5 positional arguments
+    # pylint: disable=R0913
+    # pylint: disable=R0917
+
+    button = customtkinter.CTkButton(
+        master=frame,
+        width=width,
+        text=text,
+        command=command,
+    )
+
+    if col_span:
+        button.grid(row=row_index, columnspan=3, pady=10, padx=10)
+    else:
+        button.grid(row=row_index, column=col_index, pady=10, padx=10)
+
+    if disabled:
+        button.configure(state="disabled")
+
+    return button
+
+
+def create_checkbox(
+    frame: customtkinter.CTkFrame,
+    text: str,
+    row_index: int,
+    col_index: int,
+    command: Callable[[], None],
+) -> customtkinter.CTkCheckBox:
+    """WIP"""
+
+    checkbox = customtkinter.CTkCheckBox(master=frame, text=text, command=command)
+    checkbox.grid(row=row_index, column=col_index, pady=10, padx=10)
+
+    return checkbox
+
+
+def create_entry(
+    frame: customtkinter.CTkFrame,
+    variable_text: str,
+    placeholder_text: str,
+    row_index: int,
+    col_index: int,
+) -> customtkinter.CTkEntry:
+    """WIP"""
+
+    entry = customtkinter.CTkEntry(
+        master=frame,
+        width=220,
+        justify=customtkinter.LEFT,
+        textvariable=customtkinter.StringVar(value=variable_text),
+        placeholder_text=placeholder_text,
+    )
+    entry.grid(row=row_index, column=col_index, pady=10, padx=10)
+
+    return entry
+
+
+def create_progressbar(
+    frame: customtkinter.CTkFrame, row_index: int
+) -> customtkinter.CTkProgressBar:
+    """WIP"""
+
+    progressbar = customtkinter.CTkProgressBar(master=frame)
+    progressbar.grid(row=row_index, columnspan=3, pady=10, padx=10)
+    progressbar.set(0)
+
+    return progressbar
+
+
+def create_optionmenu(
+    frame: customtkinter.CTkFrame,
+    width: int,
+    values: list[str],
+    row_index: int,
+    col_index: int,
+) -> customtkinter.CTkOptionMenu:
+    """WIP"""
+
+    optionmenu = customtkinter.CTkOptionMenu(
+        frame,
+        width=width,
+        values=values,
+    )
+    optionmenu.grid(row=row_index, column=col_index, pady=10, padx=10)
+
+    return optionmenu
